d1a6898988991ee1e3beded4c0b01b016db6a167
muubii <muubi@gmail.com>
2024-02-15 12:09:36 +0100

    i create directory for binary tree and start implementing it

---
+++ Datastructure/Tree/binarytree.py
@@ -0,0 +1,22 @@
+class Tree:
+    def __init__(self, data, left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+
+    def insert(self, data):
+        if self.data == data:
+            return False
+        elif self.data > data:
+            if self.left is not None:
+                return self.left.insert(data)
+            else:
+                self.left = Tree(data)
+                return True
+        else:
+            if self.right is not None:
+                self.right.insert(data)
+            else:
+                self.right = Tree(data)
+                return True
+            

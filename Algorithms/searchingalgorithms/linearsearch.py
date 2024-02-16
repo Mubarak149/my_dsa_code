60810ca0fba1194296ea9357a37872c789a3969f
muubii <muubi@gmail.com>
2024-02-16 12:27:24 +0100

    i implement the version of recursive linear search

--- Algorithms/searchingalgorithms/linearsearch.py
+++ Algorithms/searchingalgorithms/linearsearch.py
@@ -6,6 +6,27 @@ def linearSearch(arr, target):
 
     return None
 
+#recursive version
+def linearSearch2(arr, target, ind=0):
+    targetIndex = 0
+    if len(arr) > ind:
+        if arr[ind] == target:
+            targetIndex = arr.index(target)
+            return targetIndex
+        else:
+            targetIndex = linearSearch2(arr, target,ind + 1)
+            return targetIndex
+        return targetIndex
+    else:
+        return None
+
+
+
+
 # Testing code
+
+"""
 li = [2,34,5,5,7,89,6,62,14,25,4,78,76,345,2,56,38,6,78,35,4,68]
 print(linearSearch(li,4))
+print(linearSearch2(li,68))
+"""
